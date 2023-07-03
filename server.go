fc7d1a83b653597aba491bca015ae9ef7c91c3d5
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-03 17:32:06 +0600

    stored logs to file

--- server.go
+++ server.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"gin/route"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func storeLog() {
+	f, err := os.Create("./log/server.log")
+
+	if err != nil {
+		fmt.Println("Open Log File Failed", err)
+	}
+
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	gin.ForceConsoleColor()
+}
+
 func main() {
+	storeLog()
+
 	server := gin.Default()
 
 	route.AlbumRoutes(server)
