7e0f82b943681656a8270a0367f9ad0b8fe39afa
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-06 17:21:34 +0600

    updated log file

--- server.go
+++ server.go
@@ -10,7 +10,8 @@ import (
 )
 
 func storeLog() {
-	f, err := os.Create("./log/server.log")
+	// f, err := os.OpenFile("./log/server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // Note: Append
+	f, err := os.Create("./log/server.log") // Note: Create New and Append
 
 	if err != nil {
 		fmt.Println("Open Log File Failed", err)
